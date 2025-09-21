e6ed18c889b1cad6c2f08ed673c2782b2718f004
Jecadev <Jeanmieres1502@gmail.com>
2025-09-21 15:22:54 -0400

    actualizaciones

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ let mensajes = [
     "Eres una obra de arte :3",
     "Eres fuerte e inteligente, eso me encanta de ti.",
     "Te amo mucho. 🫶",
-    "Mi diosa. 🛐"
+    "Mi diosa. 🛐",
+    "Mereces todo lo bonito de este mundo. ✨",
+    "Eres la mejor bailarina del mundo ^^ ",
+    "Nunca olvides de lo que eres capaz y no te rindas. 😇",
+    "Estaré siempre que me necesites. 👫",
+    "¡ERES MUY HERMOSA! 😻",
+    "Amo tu voz 🤌"
 ]
 
-console.log()
-
 document.addEventListener("DOMContentLoaded", ()=>{
+    document.addEventListener("load",()=>{
+        miAudio.play()
+    })
     const tocar = document.getElementById("tocar")
     const miAudio = document.getElementById("miAudio")
     const flor = document.getElementById("flor")
     const p = document.getElementById("texto")
+
+    // miAudio.play()
     
     flor.addEventListener("click", ()=>{
         tocar.style.display = "none"
-        p.textContent = mensajes[Math.floor(Math.random() * 5)]
-        setTimeout(()=>{},1000)  
-        p.style.animation()    
+        p.textContent = mensajes[Math.floor(Math.random() * 11)]
+
+        p.classList.remove("desaparecer")
+        p.classList.remove("aparecer")
+
+        setTimeout(()=>{
+            p.classList.add("aparecer")
+
+            setTimeout(()=>{
+            p.classList.remove("aparecer")
+            p.classList.add("desaparecer")
+            }, 2000)
+        },10)        
     })
 })
